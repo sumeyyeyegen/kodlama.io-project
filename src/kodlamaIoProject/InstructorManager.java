8e0a9687f43c555ab3b3edac6e04e2cd894870a4
sumeyye <sumeyyeyegen746@gmail.com>
2021-05-01 01:01:09 +0300

    Instructro manager updated.

--- src/kodlamaIoProject/InstructorManager.java
+++ src/kodlamaIoProject/InstructorManager.java
@@ -1,5 +1,12 @@
 package kodlamaIoProject;
 
-public class InstructorManager {
-
+public class InstructorManager extends UserManager {
+	public void addInstructor(User user) {
+		System.out.println(user.getFirstName() + " isimli kullanici egitmenlere eklendi.");
+	}
+	
+	@Override
+	public void delete(User user) {
+		System.out.println(user.getFirstName() + " isimli egitmen silindi.");
+	}
 }
