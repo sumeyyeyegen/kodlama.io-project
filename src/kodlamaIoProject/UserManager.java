f710fea089eb68bf215e9d91ee098987907042db
sumeyye <sumeyyeyegen746@gmail.com>
2021-05-01 01:00:56 +0300

    user manager class updated.

--- src/kodlamaIoProject/UserManager.java
+++ src/kodlamaIoProject/UserManager.java
@@ -1,5 +1,21 @@
 package kodlamaIoProject;
 
 public class UserManager {
-
+	public void addUser (User user) {
+		System.out.println(user.getFirstName() + " isimli kullanıcı eklendi.");
+	}
+	
+	public void addUser (User[] users) {
+		for (User user : users) {
+			addUser(user);
+		}
+	}
+	
+	public void delete(User user) {
+		System.out.println(user.getFirstName() + " isimli kullanici silindi.");
+	}
+	
+	public void update(User user) {
+		System.out.println(user.getFirstName() + " isimli kullanici guncellendi.");
+	}
 }
